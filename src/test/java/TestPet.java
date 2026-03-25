aac48841bd0f8d8532470911c5abfbd747a2308c
CXPOH <cxpoh1988@gmail.com>
2026-03-25 22:05:42 +0300

    Добавил Allure а автотесты

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,7 +1,13 @@
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,19 +15,31 @@ public class TestPet {
     private static final String BASE_URL = "http://5.181.109.28:9090/api/v3";
 
     @Test
+    @Feature("pet")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("Igor_Prosenkov")
     public void tetsDeleteNoneExsistentPet() {
-        Response response = given()
+        Response response = step("Отправить DELETE запрос на удаление несуществующего питомца",() ->
+            given()
                 .contentType(ContentType.JSON)
                 .header("Accept", "application/json")
                 .when()
-                .delete(BASE_URL + "/pet/9999");
+                .delete(BASE_URL + "/pet/9999"));
 
         String responseBody = response.getBody().asString();
 
-        assertEquals(200, response.getStatusCode(),
-                "Код ответа не совпал с ожидаемым " + responseBody);
-        assertEquals("Pet deleted", responseBody,
-                "Текст ошибки не совпал с ожидаемым. получен: "+ responseBody);
+            step("Проверить, что статус -код ответа == 200",() ->
+                    assertEquals(200, response.getStatusCode(),
+                "Код ответа не совпал с ожидаемым. Ответ: " + responseBody)
+        );
+
+        step("Проверить, что текст ответа 'Pet deleted'",() ->
+                assertEquals("Pet deleted", responseBody,
+                        "Текст ошибки не совпал с ожидаемым. Получен: "+ responseBody)
+
+        );
+
+
 
     }
 
